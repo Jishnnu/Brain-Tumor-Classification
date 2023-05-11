5479b135b53d99e9324af10fdd2f1837ad06c49a
Jishnnu <jishnuanil30@gmail.com>
2023-05-11 19:42:29 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Brain Tumor Classification
-[![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/Jishnnu/Emotion-Detection)](https://github.com/Jishnnu/Brain-Tumor-Classification) &ensp; [![GitHub repo size](https://img.shields.io/github/repo-size/Jishnnu/Emotion-Detection)](https://github.com/Jishnnu/Brain-Tumor-Classification) &ensp; [![Open in Gradio](https://img.shields.io/static/v1?label=Open%20in&message=Gradio&color=blueviolet)](https://huggingface.co/spaces/Jishnnu/Brain-Tumor-Detection)
+[![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/Jishnnu/Brain-Tumor-Classification)](https://github.com/Jishnnu/Brain-Tumor-Classification) &ensp; [![GitHub repo size](https://img.shields.io/github/repo-size/Jishnnu/Brain-Tumor-Classification)](https://github.com/Jishnnu/Brain-Tumor-Classification) &ensp; [![Open in Gradio](https://img.shields.io/static/v1?label=Open%20in&message=Gradio&color=blueviolet)](https://huggingface.co/spaces/Jishnnu/Brain-Tumor-Detection)
 
-This repository contains all the materials related to Case Study 2 in the book "A Guide to Applied Machine Learning for Biologists". 
+This repository contains all the materials related to Case Study 2 in the book **_A Guide to Applied Machine Learning for Biologists_**. 
 
-Please make sure that you change the existing path specification with your custom directory path in the source code.
+Please make sure that you change the existing path specification with your custom directory path in the source code. I have deployed this CNN model on Gradio. Simply click on the **_Open in Gradio_** button to check it out.
 
 CREDITS
 1.	MRI Brain Tumor Image Dataset
@@ -11,4 +11,3 @@ CREDITS
 
 2.	ResNet101
 	https://www.tensorflow.org/api_docs/python/tf/keras/applications/resnet
-
--- app.py
+++ app.py
@@ -1,53 +1,31 @@
-import gradio as gr
+
+
+import numpy as np
 import cv2
 import tensorflow as tf
-import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import load_model
+import gradio as gr
+from keras.models import Sequential
+from keras.layers import Conv2D, Flatten, Dense, MaxPooling2D, Dropout
 
-# Load the pre-trained model
-model = tf.keras.models.load_model('Trained_Model.h5')
+model = tf.keras.models.load_model('/kaggle/input/brain-tumor-model/braintumor3.h5')
 
-# Define the class labels
-class_labels = {
-    0: 'Glioma',
-    1: 'Meningioma',
-    3: 'Pituitary',
-    2: 'No tumor'
-}
+labels = ['glioma_tumor', 'meningioma_tumor', 'no_tumor', 'pituitary_tumor']
 
-# Create the image generator for preprocessing
-img_gen = ImageDataGenerator(rescale=1./255)
+def preprocess_image(image):
+    image = cv2.resize(image, (150, 150))
+    image = np.expand_dims(image, axis=0)
+    return image
 
-# Define the function to predict tumor type
-def predict_image(file):
-    # Load the image or video
-    cap = cv2.VideoCapture(file.name)
-    if cap.isOpened():
-        ret, frame = cap.read()
-        # Check if it's an image or video
-        if frame is not None:
-            # Preprocess the image            
-            frame = cv2.resize(frame, (150, 150))
-            frame = np.expand_dims(frame, axis=-1)
-            frame = np.expand_dims(frame, axis=0)
-            frame = frame.astype('float32')
-            frame = img_gen.standardize(frame)
+def predict(image):
+    image = preprocess_image(image)
+    prediction = model.predict(image)
+    label_idx = np.argmax(prediction)
+    return labels[label_idx]
 
-            # Predict the tumor type
-            prediction = model.predict(frame)
-            label = class_labels[np.argmax(prediction)]
-        else:
-            label = "No frames found in the video"
-    else:
-        label = "Could not open the file"
-    return label
+input_interface = gr.inputs.Image(shape=(150, 150))
+output_interface = gr.outputs.Label(num_top_classes=1)
 
-# Create the Gradio interface
-input_type = gr.inputs.File(label="Upload an image or video to predict the brain tumor type")
-output_type = gr.outputs.Textbox(label="Predicted Brain Tumor")
-title = "Brain Tumor Classification"
-description = "Upload an MRI scan image or video to predict the corresponding brain tumor type"
-iface = gr.Interface(fn=predict_image, inputs=input_type, outputs=output_type, title=title, description=description)
+grapp = gr.Interface(fn=predict, inputs=input_interface, outputs=output_interface, title='Brain Tumor Detection', description='Detect brain tumors from MRI images')
+
 if __name__ == '__main__':
-    iface.launch(inline=False)
+    grapp.launch(inline = False)
